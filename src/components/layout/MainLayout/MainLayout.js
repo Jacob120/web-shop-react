9bad930e872b54c89a108b09b498ce7a9cf7d435
Jakub Kozik <jakubkozik120@gmail.com>
2022-06-30 20:33:16 +0200

    Fix mobile version

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -4,38 +4,19 @@ import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import CompareProducts from '../../features/CompareProducts/CompareProducts';
-import { useEffect } from 'react';
-import { getMediaQuery, getScreenSize } from '../../../redux/settingsReducer';
+
+import { getScreenSize } from '../../../redux/settingsReducer';
 import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { DESKTOP, TABLET, MOBILE } from '../../../settings/settings';
+
+import { MOBILE } from '../../../settings/settings';
+import { isMobile } from 'react-device-detect';
 
 const MainLayout = ({ children }) => {
   const dispatch = useDispatch();
 
-  const screenSize = useSelector(getMediaQuery);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.screen.width > 767 && screenSize !== DESKTOP) {
-        dispatch(getScreenSize({ mediaQuery: DESKTOP }));
-      } else if (
-        window.screen.width < 767 &&
-        window.screen.width > 576 &&
-        screenSize !== TABLET
-      ) {
-        dispatch(getScreenSize({ mediaQuery: TABLET }));
-      } else if (window.screen.width < 576 && screenSize !== MOBILE) {
-        dispatch(getScreenSize({ mediaQuery: MOBILE }));
-      }
-    };
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [screenSize]);
+  if (isMobile) {
+    dispatch(getScreenSize({ mediaQuery: MOBILE }));
+  }
 
   return (
     <div>
